ea637e8855970d787d9c08e0ae5ac34cc759aaf3
Andrew Selvia <andrew@selvia.com>
2022-04-09 03:05:53 -0700

    Fix scripts

--- src/main/sh/run.sh
+++ src/main/sh/run.sh
@@ -1,3 +1,5 @@
 source src/main/sh/hpc/config.sh
-GPU_IP=$(sbatch --export=ALL,TASK=$1 src/main/sh/drqv2.sh | awk '{print $4;}' | xargs -I {} cat slurm-{}.out | grep GPU_IP | cut -d "=" -f2)
+SLURM_JOB_ID=$(sbatch --export=ALL,TASK=$1 src/main/sh/drqv2.sh | awk '{print $4;}')
+sleep 5
+GPU_IP=$(grep GPU_IP "slurm-$SLURM_JOB_ID.out" | cut -d "=" -f2)
 ssh -N -f -L localhost:$HPC_PORT:localhost:$GPU_PORT "$SJSU_ID@$GPU_IP"
--- src/main/sh/tensorboard.sh
+++ src/main/sh/tensorboard.sh
@@ -1,4 +1,4 @@
 source src/main/sh/hpc/config.sh
-LOCAL_PORT=18234
-ssh -N -f -L localhost:${LOCAL_PORT}:localhost:$HPC_PORT $SJSU_ID@coe-hpc1.sjsu.edu
-open http://localhost:$LOCAL_PORT
+LOCAL_URL="localhost:18234"
+ssh -i "$SSH_ID" -N -f -L $LOCAL_URL:localhost:$HPC_PORT $HPC_HOST
+open http://$LOCAL_URL
