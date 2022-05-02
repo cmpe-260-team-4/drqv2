2fe51bc964242ca049b8770e2f293ef25ff1b331
Andrew Selvia <andrew@selvia.com>
2022-05-01 18:42:29 -0700

    Increase sleep time
    With only 5 seconds, we are seeing: "grep: slurm-51037.out: No such file or directory". Perhaps there's more traffic toward the end of the semester.

--- src/main/sh/run.sh
+++ src/main/sh/run.sh
@@ -1,5 +1,5 @@
 source src/main/sh/hpc/config.sh
 SLURM_JOB_ID=$(sbatch --export=ALL,TASK=$1 src/main/sh/drqv2.sh | awk '{print $4;}')
-sleep 5
+sleep 20
 GPU_IP=$(grep GPU_IP "slurm-$SLURM_JOB_ID.out" | cut -d "=" -f2)
 ssh -N -f -L localhost:$HPC_PORT:localhost:$GPU_PORT "$SJSU_ID@$GPU_IP"
